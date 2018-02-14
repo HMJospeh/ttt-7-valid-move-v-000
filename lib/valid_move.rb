569bbab1baff4b54e89b774fd209355a9b3f24be
Humza Joseph <humza1born@gmail.com>
2018-02-14 17:32:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,6 @@ def valid_move?(board, index)
   board[index] == "X" || board[index] == "O"
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def invalid_method?(board, index)
-position_taken == valid_move?
+def new_valid_method?(board, index)
+position_taken == valid_move? 
 end
