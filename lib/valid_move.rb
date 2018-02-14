a27bbd18061613ba5467ff4f09d9ab2f588f4da2
Humza Joseph <humza1born@gmail.com>
2018-02-14 17:21:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   board[index] == "X" || board[index] == "O"
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def invalid_method(board, index)
+def invalid_method?(board, index)
 if board[index] == "X" || board[index] == "O"
 return "truthy value"
 else
